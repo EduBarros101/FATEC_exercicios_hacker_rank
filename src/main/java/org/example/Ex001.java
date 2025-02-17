3dc1f2d843ae60a336c9792eaa735c95a61befbc
Eduardo Barros <edu.barros101@gmail.com>
2025-02-17 14:55:32 -0300

    Melhoras no primeiro exercício.

--- src/main/java/org/example/Ex001.java
+++ src/main/java/org/example/Ex001.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 public class Ex001 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    
+
+    System.out.print("Insert the first integer: ");
     int a = scanner.nextInt();
+    System.out.print("Insert the second integer: ");
     int b = scanner.nextInt();
+    System.out.print("Insert the third integer: ");
     int c = scanner.nextInt();
-    
+
+    scanner.close();
+
+    System.out.print("First integer: ");
     System.out.println(a);
+    System.out.print("Second integer: ");
     System.out.println(b);
+    System.out.print("Third integer: ");
     System.out.println(c);
   }
 }
