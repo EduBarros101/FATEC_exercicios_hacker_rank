396077a21e40081849f1f9d59ea28e0a18ab19dc
Eduardo Barros <edu.barros101@gmail.com>
2025-02-18 13:14:27 -0300

    Add Ex003

--- src/main/java/org/example/Ex003.java
+++ src/main/java/org/example/Ex003.java
@@ -6,6 +6,7 @@ public class Ex003 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     
-  // Estava com muito sono. Parei por aqui para não continuar lendo sem entender nada.
+    String string = scanner.next();
+    double
   }
 }
