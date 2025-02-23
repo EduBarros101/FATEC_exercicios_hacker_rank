b5c9c94a2b6570cb55a259bf9122d5015209be82
Eduardo Barros <edu.barros101@gmail.com>
2025-02-23 20:24:34 -0300

    Add Task description.

--- src/main/java/org/aula/Ex001.java
+++ src/main/java/org/aula/Ex001.java
@@ -1,3 +1,13 @@
+/*Task
+In this challenge, you must read
+
+integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
+
+Input Format
+
+There are
+lines of input, and each line contains a single integer.*/
+
 package org.aula;
 
 import java.util.Scanner;
