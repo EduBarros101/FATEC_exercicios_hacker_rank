26220380cfea0de26e63a49de574812723aa583c
Eduardo Barros <edu.barros101@gmail.com>
2025-03-23 22:35:25 -0300

    User entry changes

--- src/main/java/org/aula/Ex007.java
+++ src/main/java/org/aula/Ex007.java
@@ -9,12 +9,16 @@ public class Ex007 {
     
     Scanner scanner = new Scanner(System.in);
     
+    System.out.print("Digite a quantidade de casos de teste: ");
     int q = scanner.nextInt();
     
     for (int i = 0; i < q; i++) {
       
+      System.out.print("Digite o valor de a: ");
       int a = scanner.nextInt();
+      System.out.print("Digite o valor de b: ");
       int b = scanner.nextInt();
+      System.out.print("Digite o valor de n: ");
       int n = scanner.nextInt();
       
       int result = a;
